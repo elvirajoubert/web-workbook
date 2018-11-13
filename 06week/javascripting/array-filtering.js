22429ef55875faca924961f186709f2f5884da97
Elvira Joubert <reagent01@gmail.com>
2018-11-13 16:17:46 -0600

    working on jschallenge

--- 06week/javascripting/array-filtering.js
+++ 06week/javascripting/array-filtering.js
@@ -1,7 +1,7 @@
-var numbers = [1,2,3,4,5,6,7,,8,9,10];
+var numbers = [1,2,3,4,5,6,7,8,9,10];
 var evenNumbers = [numbers % 2];
 
-function evenNumbers(numbers) {
-    return number % 2 === 0;
+var filtered = number.filter(function (numbers) {
+    return (number % 2 === 0);
 }
 console.log(number.filter(evenNumbers));
