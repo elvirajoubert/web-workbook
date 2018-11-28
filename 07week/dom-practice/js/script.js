11a76c1956d6750c1d5a2b99ee5cac8615325e7b
Elvira Joubert <reagent01@gmail.com>
2018-11-28 16:45:44 -0600

    shopping cart final

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,67 +1,72 @@
 'use strict';
-window.onload = function() {
-  updateCounter();
-  insertText();}
-  
-window.onload = function(){
-  let count = document.getElementsByTagName('ul');
-  console.log(count[0].children.length);
-  
-  let newh2 = document.createElement('h2');
-  newh2.innerHTML= "You have " + count[0].children.length + " items in your shopping cart";
-  document.getElementsByTagName('h1')[0].after(newh2);
-  
-  createNewAddField();
-}
-//add input
-  function createNewAddField(){
-    let newinput = document.createElement('input');
-    newinput.setAttribute("id", 'item-text');
-    document.getElementsByTagName('ul')[0].after(newinput);
-    let inputField = document.getElementById('item-text');
-    inputField.setAttribute("placeholder", "Type to add Item");
-    
-    let newbutton = document.createElement('button');
-    newbutton.setAttribute("id", 'add-button');
-    document.getElementById('item-text').after(newbutton);
-    newbutton.innerHTML = "add item";
-   // list1[0].after(buttonAdd);
-    newbutton.onclick = addItem;
-    updateItems();
-  }    
-//Add more items into the cart list//
-    
-function addItem(event){
-  let newItemText = document.getElementById("item-text").value;
-  let newItem = document.createElement("li");
-  newItem.innerHTML = newItemText;
-  document.getElementById("shopping-list").appendChild(newItem);
-  addRemoveButton(newItem);
-}
-  
 
-  //add remove button//
-  function addRemoveButton(appendElement){
-    console.log(appendElement);
-  
-    let removeButton = document.createElement("button");
-    removeButton.innerHTML = "Remove";
-    removeButton.onclick = deleteItem;
-    appendElement.appendChild(removeButton);
-  }
-    
-    function deleteItem(event){
-      this.parentNode.remove();
-      updateItems();
-    } 
-          
-  function updateCounter() {
+window.onload = function () {
+  updateCounter();
+  insertText();
+}
+
+//create items from input text
+function insertText() {
+  var addNewItems = document.createElement('input');
+  addNewItems.setAttribute('type', 'text');
+  addNewItems.setAttribute('placeholder', 'Type in an Item');
+  var list = document.getElementsByTagName('ul');
+  list[0].after(addNewItems);
+  addNewItems.setAttribute('id', 'input-text');
+
+  var buttonAdd = document.createElement('button');
+  buttonAdd.innerHTML = 'add item';
+  buttonAdd.setAttribute('id', 'addButton');
+  var list1 = document.getElementsByTagName('input');
+  list1[0].after(buttonAdd);
+  buttonAdd.onclick = addItem;
+}
+
+//add items to cart
+function addItem(event) {
+  var newItemText = document.getElementById('input-text');
+  var newList = document.createElement('li');
+  newList.innerHTML = newItemText.value;
+  console.log(newList);
+  document.getElementsByTagName('ul')[0].appendChild(newList);
+  document.getElementById('input-text').value = "";
+  updateItems();
+  removeItem(newList);
+}
+//remove items from cart
+function removeItem(items) {
+  var buttonRemove = document.createElement('button');
+  buttonRemove.innerHTML = 'remove item';
+  items.appendChild(buttonRemove);
+  buttonRemove.onclick = eraseItem;
+}
+
+//erases the items from the cart on click above
+function eraseItem(event) {
+  this.parentNode.remove();
+  updateItems();
+}
+
+function removeExsisting() {
+  var buttonRemove2 = document.createElement('button');
+  buttonRemove2.innerHTML = 'remove item';
+
+
+}
+
+function eraseExsisting(event) {
+  this.parentNode.remove();
+  updateItems();
+}
+
+//updates the counter above
+function updateCounter() {
   var newh2 = document.createElement('h2');
   var listCount = document.getElementsByTagName('ul')[0].children.length;
   newh2.innerHTML = 'You have ' + listCount + ' items in your shopping cart.';
   document.getElementsByTagName('h1')[0].after(newh2);
   newh2.setAttribute('id', 'counter');
-  updateItems();
+  alert('You have ' + listCount + ' items in your shopping cart.')
 }
 
 //updates text on page
@@ -70,9 +75,3 @@ function updateItems() {
   var listCount = document.getElementsByTagName('ul')[0].children.length;
   counter.innerHTML = 'You have ' + listCount + ' items in your shopping cart.';
 }
-      
-    
-    
- let newh2 = document.createElement('h2');
-  newh2.innerHTML= "You have " + count[0].children.length + " items in your shopping cart";
-  document.getElementsByTagName('h1')[0].after(newh2);
