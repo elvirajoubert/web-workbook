fe3823d1f7c2d2cb84398d1b01959f822cb73747
Elvira Joubert <reagent01@gmail.com>
2018-12-05 14:44:40 -0600

    update-complete

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,34 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+
+let $currentStack;
+let $current = null;
+$('[data-stack]').click(function() {
+  if ($current === null && $(this).children().length > 0) {
+    $current = $(this).children().last().detach();
+    $currentStack=$(this);
+  }else if($(this).children().length > 0) {
+    if($(this).children().last().data('block') < ($current.data('block'))) {
+      //new code
+      $current.appendTo($currentStack);
+      $current = null;
+    }else if($(this).children().last().data('block') > ($current.data('block'))) {
+      $(this).append($current);
+      $current = null;
+    }
+  }else if ($(this).children().length === 0) {
+    $current.appendTo($(this));
+    $current = null;
+  }
+
+  var $checkWinner = $('[data-stack="3"]').children().length;
+  if ($checkWinner === 4) {
+    alert('You win!');
+  };
+})
+
+
+//announce winner
+
 });
