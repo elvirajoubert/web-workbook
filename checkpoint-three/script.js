74ffb71edacd4af77ee1005bbcc958c7fdfd374f
Elvira Joubert <reagent01@gmail.com>
2018-12-12 19:58:37 -0600

    need help with js

--- checkpoint-three/script.js
+++ checkpoint-three/script.js
@@ -1,17 +1,30 @@
 'use strict'
+function checkIfComplete() {
 
-$('#go').click(function () {
-    function checkIfComplete() {
-        if (isComplete == false) {
-            isComplete = true;
-        } else {
-            place = 'second';
-        }
+    // console.log('checking if complete');
+
+    if (isComplete == false) {
+        isComplete = true;
+    } else {
+        place = 'second';
     }
+}
+
+$('#go').onclick(function () {
+    console.log('onclick function');
+    $('marvel').animate ()
+        $('#figure1').animate({left: '500px'});
+        $('#figure2').animate({left: '500px'});
+        checkIfComplete();
+    
 });
 
-var marvelWidth = $('.marvel').width();
-var marvelWidth = $('figure1').width() - marvelWidth;
+//  var wonderWomanPixels = Math.floor((Math.random() * 3000) + 1);
+//  var airesPixels = Math.floor((Math.random() * 3000) + 1);
+
+
+var marvelWidth = $(".marvel").width();
+var marvelWidth = $("figure1").width() - marvelWidth;
 var trackWidth = $(window).width() - marvelWidth;
 var raceTime1 = Math.floor((Math.random() * 3000) + 1);
 var raceTime2 = Math.floor((Math.random() * 3000) + 1);
@@ -21,24 +34,25 @@ var place = 'first';
 
 //figure animation
 
-$('#figure1').animate({
-    left: trackWidth
-}, raceTime1, function () {
-    checkIfComplete();
-    $('raceInfo1 span').text('Finished in ' + ' place  and clocked in at ' + raceTime1 + ' milliseconds!');
+$("#figure1").click(function(){
+$('#figure1').animate({left: trackWidth},
+raceTime1, function () {
+checkIfComplete();
+$('raceInfo1 span').text(' Finished in ' + ' place  and clocked in at ' + raceTime1 + ' milliseconds! ');
+        
+});
+})
+
+$('#figure2').animate({left: trackWidth}, 
+raceTime2, function () {
+checkIfComplete();
+$('raceInfo2 span').text(' Finished in ' + ' place  and clocked in at ' + raceTime2 + ' milliseconds! ');
 
 });
-$('#figure2').animate({
-    left: trackWidth
-}, raceTime2, function () {
-    checkIfComplete();
-    $('raceInfo2 span').text('Finished in ' + ' place  and clocked in at ' + raceTime2 + ' milliseconds!');
-
-});
-// });
 
 //reset button
 
 $('#reset').click(function () {
     $('.marvel').css('left', '0');
-    $('.raceInfo span').text('')
+    $('.raceInfo span').text('');
+});
