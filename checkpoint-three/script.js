f71dea891868733558d5f13e8c70953375965d98
Elvira Joubert <reagent01@gmail.com>
2018-12-12 20:11:07 -0600

    need help with mathrandom function

--- checkpoint-three/script.js
+++ checkpoint-three/script.js
@@ -10,21 +10,22 @@ function checkIfComplete() {
     }
 }
 
-$('#go').onclick(function () {
+ $('#go').click(function () {
     console.log('onclick function');
     $('marvel').animate ()
-        $('#figure1').animate({left: '500px'});
-        $('#figure2').animate({left: '500px'});
-        checkIfComplete();
+    $('#figure1').animate({left: '1000px'});
+    $('#figure2').animate({left: '1200px'});
     
-});
+     checkIfComplete();
+    
+ });
 
 //  var wonderWomanPixels = Math.floor((Math.random() * 3000) + 1);
 //  var airesPixels = Math.floor((Math.random() * 3000) + 1);
 
 
 var marvelWidth = $(".marvel").width();
-var marvelWidth = $("figure1").width() - marvelWidth;
+var marvelWidth = $(".figure1").width() - marvelWidth;
 var trackWidth = $(window).width() - marvelWidth;
 var raceTime1 = Math.floor((Math.random() * 3000) + 1);
 var raceTime2 = Math.floor((Math.random() * 3000) + 1);
